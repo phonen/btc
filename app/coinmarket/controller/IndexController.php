09edd0a3616a2be4c4a48d572983e5e5a92a9c27
phonen <pioul@tom.com>
2018-03-13 08:39:10 +0800

    1

---
+++ app/coinmarket/controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+namespace app\coinmarket\controller;
+
+use cmf\controller\HomeBaseController;
+use think\Db;
+
+class IndexController extends HomeBaseController
+{
+    public function index()
+    {
+        Db::connect();
+        return $this->fetch(':index');
+    }
+}
