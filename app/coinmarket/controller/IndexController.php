68e17a927d37113b04884281b4d30a475342f86b
phonen <pioul@tom.com>
2018-03-30 15:39:40 +0800

    1

--- app/coinmarket/controller/IndexController.php
+++ app/coinmarket/controller/IndexController.php
@@ -34,25 +34,28 @@ class IndexController extends HomeBaseController
     public function h48()
     {
 
-        $coinmarket = Db::connect('db_coinmarket')->name("coinmarket_48h")->field("id,symbol , price,volume,price-price12 as price12,volume-volume12 as volume12,price-price48 as price48,volume-volume48 as volume48")->order("volume12 desc")->select();
-        foreach ($coinmarket as $coin){
-            if($coin['id'] == "bitcoin") {
-                $bitcoin = $coin['price'];
-                break;
+        $param = $this->request->param();
+        if(isset($param['sort']) && isset($param['order'])){
+            $coinmarket = Db::connect('db_coinmarket')->name("coinmarket_48h")->field("id,symbol , price,volume,price-price12 as price12,volume-volume12 as volume12,price-price48 as price48,volume-volume48 as volume48")->order("volume12 desc")->select();
+            foreach ($coinmarket as $coin){
+                if($coin['id'] == "bitcoin") {
+                    $bitcoin = $coin['price'];
+                    break;
+                }
             }
-        }
-        $coins = array();
-        foreach($coinmarket as $coin){
-            $coin['price12'] = round($coin['price12'],4);
-            if($coin['price'] != 0)
-            $coin['price12p'] = round($coin['price12'] /$coin['price']*100,2) . "%";
-            $coin['volume12'] = round($coin['volume12']/$bitcoin,2);
-            $coin['volume48'] = round($coin['volume48']/$bitcoin,2);
+            $coins = array();
+            foreach($coinmarket as $coin){
+                $coin['price12'] = round($coin['price12'],4);
+                if($coin['price'] != 0)
+                    $coin['price12p'] = round($coin['price12'] /$coin['price']*100,2) . "%";
+                $coin['volume12'] = round($coin['volume12']/$bitcoin,2);
+                $coin['volume48'] = round($coin['volume48']/$bitcoin,2);
 
-            array_push($coins,$coin);
+                array_push($coins,$coin);
+            }
+            echo json_encode($coins);
         }
-        echo json_encode($coins);
-
+        else return $this->fetch(':h48');
 
     }
     public function h12()
