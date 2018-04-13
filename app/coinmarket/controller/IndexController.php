cbe690eed720fdc01d34c5a3a503bcd3cc48c6bf
phonen <pioul@tom.com>
2018-04-13 10:37:55 +0800

    1

--- app/coinmarket/controller/IndexController.php
+++ app/coinmarket/controller/IndexController.php
@@ -44,7 +44,7 @@ class IndexController extends HomeBaseController
             $limit = isset($param['limit'])?$param['limit']:10;
             $total = Db::connect('db_coinmarket')->name("coinmarket_48h")->count();
             $prices = Db::connect('db_coinmarket')->name("coinmarket_last")->field("price_usd as price")->where("coinid='bitcoin'")->find();
-            $coinmarket = Db::connect('db_coinmarket')->name("coinmarket_48h")->field("id,symbol , price,volume,price-price12 as price12,volume-volume12 as volume12,price-price48 as price48,volume-volume48 as volume48,price-price24 as price24,volume-volume24 as volume24")->order($sort . " " . $order)->limit($offset,$limit)->select();
+
             /*
             foreach ($coinmarket as $coin){
                 if($coin['id'] == "bitcoin") {
@@ -55,7 +55,24 @@ class IndexController extends HomeBaseController
             */
             if($prices){
                 $bitcoin = $prices['price'];
+                $coinmarket = Db::connect('db_coinmarket')->name("coinmarket_48h")->field("id,symbol , price,volume,price-price12 as price12,(price-price12)/price as price12p,(price-price24)/price as price24p,volume-volume12 as volume12,price-price48 as price48,volume-volume48 as volume48,price-price24 as price24,volume-volume24 as volume24")->order($sort . " " . $order)->limit($offset,$limit)->select();
+                $coins = array();
+                foreach($coinmarket as $coin){
+                    $coin['price12'] = round($coin['price12'],4);
+                    $coin['price12p'] = round($coin['price12p']*100,2) . "%";
+                    $coin['price24p'] = round($coin['price24p']*100,2) . "%";
+
+
+                    $coin['volume'] = round($coin['volume']/$bitcoin,2);
+                    $coin['volume12'] = round($coin['volume12']/$bitcoin,2);
+                    $coin['volume24'] = round($coin['volume24']/$bitcoin,2);
+                    $coin['volume48'] = round($coin['volume48']/$bitcoin,2);
+
+                    array_push($coins,$coin);
+                }
+
             }
+            /*
             $coins = array();
             foreach($coinmarket as $coin){
                 $coin['price12'] = round($coin['price12'],4);
@@ -71,6 +88,7 @@ class IndexController extends HomeBaseController
 
                 array_push($coins,$coin);
             }
+            */
             echo json_encode(array("total"=>$total,"rows"=>$coins));
         }
         else return $this->fetch(':h48');
